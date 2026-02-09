1bf77a9e3ffd3259c07ce5189af6cb6f0c24a3e8
chehachraf <achrafchehboun@outlook.com>
2026-02-09 09:58:04 +0100

    add the navbar and configure the home page with routing

---
+++ src/app/feature/home/home-component/home-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from '../../../shared/components/navbar-component/navbar-component';
+
+@Component({
+  selector: 'app-home-component',
+  imports: [
+    NavbarComponent
+  ],
+  templateUrl: './home-component.html',
+  styleUrl: './home-component.css',
+})
+export class HomeComponent {
+
+}
