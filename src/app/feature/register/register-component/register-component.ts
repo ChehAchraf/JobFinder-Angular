456b3988de38c7395f5b597c97d929ea9cf8c354
chehachraf <achrafchehboun@outlook.com>
2026-02-10 15:57:55 +0100

    add the validation in the form

--- src/app/feature/register/register-component/register-component.ts
+++ src/app/feature/register/register-component/register-component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
+  standalone : true,
   templateUrl: './register-component.html',
   styleUrl: './register-component.css',
 })
 export class RegisterComponent {
+
+  private readonly fb = inject(FormBuilder)
+
+  registerForm = this.fb.group({
+    username: ['',[Validators.required,Validators.minLength(4)]],
+    email : ['', [Validators.email, Validators.required]],
+    password : ['' , [Validators.required, Validators.minLength(6)]]
+  })
+
+  get username(){
+    return this.registerForm.controls.username;
+  }
+
+  get email(){
+    return this.registerForm.controls.email;
+  }
+
+  get password(){
+    return this.registerForm.controls.password;
+  }
+
+
+
 
 }
