4d19de4b8217d217abadaa6fccba4e6d9603682a
chehachraf <achrafchehboun@outlook.com>
2026-02-10 14:56:52 +0100

    add form to login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,15 @@ export const routes: Routes = [
     path : '',
     loadComponent : ()=> import('./feature/home/home-component/home-component')
       .then((c)=> c.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./feature/login/login-component/login-component')
+      .then((c)=>c.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent : () => import('./feature/register/register-component/register-component')
+      .then((c)=>c.RegisterComponent)
   }
 ];
