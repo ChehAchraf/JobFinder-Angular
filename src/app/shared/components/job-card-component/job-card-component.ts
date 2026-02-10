740181e19f54a9751bb1d315400f948d26b95c1e
chehachraf <achrafchehboun@outlook.com>
2026-02-10 14:29:32 +0100

    adding a custom pipe to truncate the text

--- src/app/shared/components/job-card-component/job-card-component.ts
+++ src/app/shared/components/job-card-component/job-card-component.ts
@@ -1,11 +1,13 @@
 import {Component, input} from '@angular/core';
 import {Job} from '../../../core/model/job.model';
 import {DatePipe} from '@angular/common';
+import {TruncatePipe} from '../../pipes/truncate-pipe';
 
 @Component({
   selector: 'app-job-card-component',
   imports: [
-    DatePipe
+    DatePipe,
+    TruncatePipe
   ],
   templateUrl: './job-card-component.html',
   styleUrl: './job-card-component.css',
---
+++ src/app/shared/pipes/truncate-pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string,): string {
+    if(value.length > 20){
+      return value.slice(0,120);
+    }
+    return value
+  }
+
+}
